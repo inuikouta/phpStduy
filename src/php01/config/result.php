d9e6780f068769a369f4db593f8210bb863dc1ec
nuinui <inuikouta0423@gmail.com>
2025-02-26 23:55:12 +0900

    初回コミット

--- src/php01/config/result.php
+++
@@ -1,8 +0,0 @@
-<?php
-$name = htmlspecialchars($_POST['name'], ENT_QUOTES);
-$price = htmlspecialchars($_POST['price'], ENT_QUOTES);
-$number = htmlspecialchars($_POST['number'], ENT_QUOTES);
-
-print "あなたの名前は、" . $name . "<br>";
-print "注文した商品は、" . $price . "<br>";
-print "注文数は、" . $number . "<br>";
--- src/php01/index04.php
+++
@@ -1,4 +0,0 @@
-<?php
-$a = 7;
-$result = ($a == 7) ? "TRUE" : "FALSE";
-echo $result;
